a65a7c22c8edfe978d6e2a92d940851a52793e01
pashupati-git <pashupatic.github@gmail.com>
2026-05-27 15:23:39 +0545

    injecting statrd from data sources

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -5,7 +5,6 @@
 //Registration order matters: register dependencies before dependents
 // DataSource -> Repository -> UseCases ->BLoc
 /// The `initDependencies` function sets up dependency injection using GetIt for data sources,
-/// repositories, use cases, and BLoCs in a Dart application.
 
 import 'package:bloc_todo/features/data/datasources/todo_local_datasource.dart';
 import 'package:bloc_todo/features/data/repositories/todo_repositories_impl.dart';
